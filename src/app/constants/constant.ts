c0f0caf49ebf72e19f88e2627f71dedbde708fc2
ShubhamBhardwaj218 <shubhambhardwaj218@gmail.com>
2024-03-21 00:56:27 +0530

    Removed API Keys

--- src/app/constants/constant.ts
+++ src/app/constants/constant.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyDIknVazHvbTZW2hw7bmprxHfDmI_wEH00",
-    authDomain: "angular-17-crud-2be95.firebaseapp.com",
-    databaseURL: "https://angular-17-crud-2be95-default-rtdb.firebaseio.com",
-    projectId: "angular-17-crud-2be95",
-    storageBucket: "angular-17-crud-2be95.appspot.com",
-    messagingSenderId: "234392250682",
-    appId: "1:234392250682:web:eacd99f24bb2ad0587c803",
-    measurementId: "G-DYHGM8BXX8"
+    apiKey: "FakeAPIKEY",
+    authDomain: "FakeAPI.firebaseapp.com",
+    databaseURL: "https://FakeAPI-rtdb.firebaseio.com",
+    projectId: "FakeAPI-2be95",
+    storageBucket: "FakeAPI.appspot.com",
+    messagingSenderId: "FakeAPI",
+    appId: "FakeAPI",
+    measurementId: "FakeAPI"
   };
